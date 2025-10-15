751816bab3f35e2ae184b59d7961cfa2c217173a
Kiara Reyes <kiarareyesleiva@gmail.com>
2025-10-15 20:55:39 +0200

    15-10-2025

--- FastAPI_reyes/FastAPI_Reyes.py
+++ FastAPI_reyes/FastAPI_Reyes.py
@@ -1,3 +1,11 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+
+users = []
+
+@app.post("/api/users", response_model=dict)
+async def create_user(user_data: dict):
+
+    users.append(user_data)
+    return users
